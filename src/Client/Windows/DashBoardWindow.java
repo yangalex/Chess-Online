ed8ddf6e8d4a57d53b1d1284427e96799b2e58dc
yangalex <yangalex@usc.edu>
2015-11-13 17:53:31 -0800

    Added a JList and setOnlinePlayers method to DashBoardWindow

--- src/Client/Windows/DashBoardWindow.java
+++ src/Client/Windows/DashBoardWindow.java
@@ -2,8 +2,13 @@ package Client.Windows;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.util.Vector;
 
+import javax.swing.JList;
 import javax.swing.JPanel;
+
+import Client.ChessClient;
+import Server.Player;
 
 public class DashBoardWindow extends JPanel {
 	private static final long serialVersionUID = -8690011202797301705L;
@@ -11,15 +16,23 @@ public class DashBoardWindow extends JPanel {
 	/// Connections
 	private GameBoardWindow gameboard;
 	private ClientPanelWindow cpw;
+	private ChessClient chessClient;
+	
+	// Components
+	private JList playersList;
 	
 	public DashBoardWindow(ClientPanelWindow cpw) {
 		this.cpw = cpw;
+		chessClient = cpw.getChessClient();
+		requestOnlinePlayers();
 		initializeElements();
 		createGUI();
 	}
 	
+	
 	private void initializeElements() {
-		gameboard = new GameBoardWindow();
+//		gameboard = new GameBoardWindow();
+		playersList = new JList<String>();
 	}
 	
 	private void createGUI() {
@@ -30,7 +43,24 @@ public class DashBoardWindow extends JPanel {
 		
 		add(gameboard, gbc);
 		
-		
 	}
+
+	private void setOnlinePlayers(Vector<Player> onlinePlayers) {
+		int numPlayers = onlinePlayers.size();
+		String[] data = new String[numPlayers];
+		
+		for (int i=0; i < onlinePlayers.size(); i++) {
+			data[i] = onlinePlayers.get(i).getUsername();
+		}
+
+		playersList.setListData(data);
+		playersList.repaint();
+	}
+	
+	//////////// Server Communication //////////////
+	private void requestOnlinePlayers() {
+		chessClient.requestOnlinePlayers();
+	}
+
 
 }
